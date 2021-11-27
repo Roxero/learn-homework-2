1d781d9214d479fb1c89d7ede25fea7f2bc08d97
V. Golov <guru@indiancode.ru>
2021-11-27 23:38:50 +0300

    Done csv

--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -10,12 +10,28 @@
 
 """
 
+import csv
+
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    dump = [
+        {'name': 'Jose  Marquez Alcantara', 'age': 57, 'job': 'Mexican actor'},
+        {'name': 'Elizabeth II', 'age': 95, 'job': 'Brittish pensioner'},
+        {'name': 'Pikachu', 'age': 28, 'job': 'Yellow pokemon'},
+        {'name': 'Gal Gadot', 'age': 36, 'job': 'Superwoman actress'},
+        {'name': 'Xi Wei', 'age': 29, 'job': 'Chinese singer'},
+        {'name': 'Petr Ivanov', 'age': 17, 'job': 'University student'},
+        {'name': 'Simon Vixelshtein', 'age': 77, 'job': 'Too much money Inc. co-founder'},
+        {'name': 'Maria Perre', 'age': 25, 'job': 'Photomodel'},
+        {'name': 'Yokotsuma Karao Nagata', 'age': 44, 'job': 'even Yakuzas don\'t know'}
+    ]
+
+    with open('./dump.csv', 'w', encoding='UTF-8') as f:
+        fields = ['name', 'age', 'job']
+        writer = csv.DictWriter(f, fields, delimiter=';')
+        writer.writeheader()
+        for person in dump:
+            writer.writerow(person)
+
 
 if __name__ == "__main__":
     main()
